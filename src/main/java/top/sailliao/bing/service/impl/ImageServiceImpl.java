68bdd45106f0e9ba61f0a08709e63762c09c956b
tanglei <1601664015@qq.om>
2020-12-31 16:48:07 +0800

    按照时间倒序

--- src/main/java/top/sailliao/bing/service/impl/ImageServiceImpl.java
+++ src/main/java/top/sailliao/bing/service/impl/ImageServiceImpl.java
@@ -4,6 +4,7 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
 import top.sailliao.bing.entity.Image;
 import top.sailliao.bing.mapper.ImageMapper;
 import top.sailliao.bing.service.ImageService;
@@ -28,7 +29,9 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public PageInfo page(Integer pageNum, Integer pageSize) {
         PageHelper.startPage(pageNum, pageSize);
-        List<Image> list = imageMapper.selectAll();
+        Example example = new Example(Image.class);
+        example.setOrderByClause("date desc");
+        List<Image> list = imageMapper.selectByExample(example);
         return new PageInfo(list);
     }
 }
